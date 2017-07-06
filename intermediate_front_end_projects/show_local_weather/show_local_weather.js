90b19e3272e1b9c88fce240d75da730f9face215
Mike Mullen <mmullen227@gmail.com>
2017-07-06 10:56:17 -0400

    temperatures retrieved from api

--- intermediate_front_end_projects/show_local_weather/show_local_weather.js
+++ intermediate_front_end_projects/show_local_weather/show_local_weather.js
@@ -11,26 +11,41 @@ var apiKey = "2fdc967638822edf6f3803cfc783f9a4";
           // variables that store the users latitude and longitude
           var lat = position.coords.latitude;
           var long = position.coords.longitude;
-          
-          $("#data").html("Latitude: " + lat + 
-          "<br>Longitude: " + long); 
-          
+    
           // The API with the latitude and longitude 
-          var api = "https://api.darksky.net/forecast/" + apiKey + "/" + lat + "," + long;
+          // Needed to use a proxy due to https
+          var api = "https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/" + apiKey + "/" + lat + "," + long;
           
           // JSON call to retrieve data from DarkSky api
           $.getJSON(api, function(data) {
-            var city = data.timezone;
-            console.log(city);
-            console.log(api);
-          });
+            // temperature in Fahrenheit
+            var tempF = Math.round(data.currently.temperature);
+            // temperature in Celsius
+            var tempC = Math.round((tempF - 32) / (9/5));
             
+            $("#fahr").html(tempF);
+            $("#cel").html(tempC);
             
+  
           });
-        }else { 
+          });  
+        } else { 
           $("data").html = "Geolocation is not supported by this browser.";
-      } 
-  };
+      };
+  
+  }
   getLocation();
 });
 
+
+
+/*
+  .done(function(data) {
+  })
+  .fail(function( jqxhr, textStatus, error ) {
+    var err = textStatus + ", " + error;
+    console.log( "Request Failed: " + err );
+  })
+  .always(function() {
+    console.log( "complete" );
+*/
