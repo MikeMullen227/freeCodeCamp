a3e5f9137b51f9d4cf8383d8faa775bb8ac3b717
Mike Mullen <mmullen227@gmail.com>
2017-08-06 00:23:55 -0400

    js refactored, keypress now works on first attempt because of callback function

--- intermediate_front_end_projects/wikipedia_viewer/wikipedia_viewer.js
+++ intermediate_front_end_projects/wikipedia_viewer/wikipedia_viewer.js
@@ -1,11 +1,15 @@
 //jQuery
 $(document).ready(function() {
-  $("#submit").click(function() {
+  
+    // callback function to be passed into click and keypress jquery objects
+    var callback = function() {
+      
     // get the value entered in the search field and store in searchTerm
     var searchTerm = $("#search").val();
+      
     // concatenate the search term entered by user to the wiki url
     var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchTerm + "&format=json&callback=?";
-    
+      
     // make ajax request to wiki api to get search results
     $.ajax({
         type: "GET",
@@ -16,16 +20,30 @@ $(document).ready(function() {
           
         // clear results before each search  
         $("#results").empty();
+          
           // loop through json array to add results to ul
           for(var i = 0; i < data[1].length; i++) {
               $("#results").prepend("<li><a target='_blank' href= " + data[3][i] + ">" + data[1][i] + "</a>" + "<p>" + data[2][i] + "</p></li>"); 
           }
-         
+          
+          // clear search field after each search
+          $("#search").val('');
+          
         },
         error: function() {
           console.log("error");
         }
     })
-  })
+
+  }
+   
+  // on click and keypress both work on first search attempt since callback was created first
+  $("#submit").click(callback);
+ 
+  $("#search").keypress(function(e) {
+    if (e.which == 13) {
+      callback();
+    }
+});
 });
 
