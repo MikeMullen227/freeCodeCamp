2478a1623a384c94726163e144b6a91655462c12
Mike Mullen <mmullen227@gmail.com>
2017-09-19 13:52:55 -0400

    functionality completed, styling needed

--- intermediate_front_end_projects/twitch_api/twitch_api.js
+++ intermediate_front_end_projects/twitch_api/twitch_api.js
@@ -3,39 +3,39 @@ $(document).ready(function() {
   
   var clientID = '3dthbe7t8zuydf6zdor03wjrfimxhw';
   
-  //var url = 'https://wind-bow.gomix.me/twitch-api/streams/freecodecamp?callback=?';
-  /*
-  $.getJSON('https://api.twitch.tv/kraken/streams/esl_sc2?clientID=' + client_id).done(function(data) {
-      //console.log(data);
-    if(data.stream === null) {
-      $('#status').html('Offline');
-    } else {
-      $('#status').html('Online');
-    }
-  })
- */
+  var twitchURL = 'https://api.twitch.tv/kraken/';
   
-  for(var i = 0; i < users.length; i++) {
 
-    $.ajax({
-          type: 'GET',
-          url: 'https://api.twitch.tv/kraken/channels/' + users[i],
-          headers: {
-          'Client-ID': clientID
-          },
-             success: function(data) {
-                 console.log(data);
-                  if(data.stream === null) {
-                      $('#fccStatus').html('Offline')
-                  } else {
-                      $('#fccStatus').html('Online')
-                  };
-             }, 
-             error: function(er) {
-               
-             }
-      });
-    
-  };
-  
+      for(var i = 0; i < users.length; i++) {
+
+        $.ajax({
+              type: 'GET',
+              url: twitchURL + 'channels/' + users[i],
+              headers: {
+              'Client-ID': clientID
+              },
+                 success: function(data1) {
+                     console.log(data1);
+                   var logo = data1.logo !== null ? data1.logo : "http://web.vmc3.com/projects/bufs2016/branch/army/logos/NoLogo.jpg";
+                   var name = data1.display_name;  
+                   var status = data1.status;
+
+                       $.ajax({
+                         type: 'GET',
+                         url: twitchURL + 'streams/' + data1.name,
+                         headers: {
+                           'Client-ID': clientID
+                         },
+                            success: function(data2) {
+                                         if(data2.stream === null) {
+                                            $('#userInfo').prepend('<div class="row">' + '<div class="col-md-4" id="logo">' + '<img src=' + logo + '>' + '</div>' + '<div class="col-md-4" id="user">' + name + '</div>' + '<div class="col-md-4" id="status">Offline</div>' + '</div>');
+                                         } else {
+                                            $('#userInfo').prepend('<div class="row">' + '<div class="col-md-4" id="logo">' + '<img src=' + logo + '>' + '</div>' + '<div class="col-md-4" id="user">' + name + '</div>' + '<div class="col-md-4" id="status">' + status + '</div>' + '</div>');
+                                         }
+                                      }  
+                       });
+                    }    
+        });
+      };
+
 });
