faca77749cf866adfeb98f3eec0f9c62d598539e
Mike Mullen <mmullen227@gmail.com>
2017-10-18 12:56:11 -0400

    session time buttons and break time buttons working, still working on timer and break functionality

--- advanced_front_end_projects/pomodoro_clock/script.js
+++ advanced_front_end_projects/pomodoro_clock/script.js
@@ -1,48 +1,106 @@
 $(document).ready(function() {
     var now;
     var interval = 0;
+    var clear = 0;
     var minutes = 25; 
     var end = new Date().getTime() + minutes*60000;
-  
-  function clear() {
-    return clearInterval(interval);
-  }
-  
-    // A function that adds minutes to the timer. Default is set to 25. 
-  
-  function timer() {
-    
-    // Get todays date and time
-     now = new Date().getTime();
-    
-    // Find the distance between now and the count down date
-    var distance = end - now;
-    console.log(distance);
-    // Time calculations for days, hours, minutes and seconds
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    console.log(minutes);
-    console.log(seconds);
-    // Need to add a zero to the beginning of seconds when they are single digits
+    var stopped;
+    var stoppedTime;
+    var newTime;
+    var alarm = $('#alarm')[0];
+    var count1 = parseInt($('#count1').html());
+    var count2 = parseInt($('#count2').html());
+    var breakTime;
+    //alarm.play();
+
+    //amount of seconds total
+
+    function sessionTimer() {
+        if (count1 === 0) {
+            //alarm.play(); 
+            clear();
+            breakTime = setInterval(breakTimer, 1000);
+        } else {
+            count1--;
+        }
+
+        function breakTimer() {
+            //$('#count2').
+        }
+        /*
     if(seconds < 10) {
       seconds = '0' + seconds;
     }
-    // Display the result in the element with class=timer"
-    $('.timer').text(minutes + ':' + seconds);
-    
-    if (distance < 0) {
-    clear();
-    }
-  }
-  
-    
+    */
 
-  $('#start').click(function() {
-    interval = setInterval(timer, 1000);
-  });
-  
-  $('#stop').click(clear);
+        // Display the result in the element with class=timer"
+        $('.timer').html(count1); 
+    };
+
+
+    //Buttons
+
+    $('#start').click(function() {
+        interval = setInterval(sessionTimer, 1000);
+    });
+
+    $('#stop').click(function() {
+        clear = clearInterval(interval);
+    });
+    //$('#reset').click()
+
+    $('#minus1').click(function() {
+        if(count1 > 1) {
+            count1--;
+            $('#count1').html(count1);
+            $('#sessionTime').html(count1 + ':00');
+        }
+    });
+
+    $('#plus1').click(function() {
+        count1++;
+        $('#count1').html(count1); 
+        $('#sessionTime').html(count1 + ':00');
+    });
+
+    $('#minus2').click(function() {
+        if(count2 > 1) {
+            count2--;
+            $('#count2').html(count2);
+        }
+    });
+
+    $('#plus2').click(function() {
+        count2++;
+        $('#count2').html(count2); 
+    });
+
 });
+/*
+$('#reset').click(function() {
+    $('#count1').html()
+});
+*/
+/*
 
+function Timer(callback, delay) {
+    var timerId, start, remaining = delay;
 
+    this.pause = function() {
+        window.clearTimeout(timerId);
+        remaining -= new Date() - start;
+    };
 
+    this.resume = function() {
+        start = new Date();
+        window.clearTimeout(timerId);
+        timerId = window.setTimeout(callback, remaining);
+    };
+
+    this.resume();
+}
+
+var timer = new Timer(function() {
+    alert("Done!");
+}, 1000);
+    */
