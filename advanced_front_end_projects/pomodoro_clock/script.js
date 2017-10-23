faee5567ebe689f7212389fd5fca449f1798e8b8
Mike Mullen <mmullen227@gmail.com>
2017-10-22 23:06:28 -0400

    session plus and minus working properly with the countdown

--- advanced_front_end_projects/pomodoro_clock/script.js
+++ advanced_front_end_projects/pomodoro_clock/script.js
@@ -1,106 +1,86 @@
 $(document).ready(function() {
-    var now;
-    var interval = 0;
-    var clear = 0;
-    var minutes = 25; 
-    var end = new Date().getTime() + minutes*60000;
-    var stopped;
-    var stoppedTime;
-    var newTime;
-    var alarm = $('#alarm')[0];
-    var count1 = parseInt($('#count1').html());
-    var count2 = parseInt($('#count2').html());
-    var breakTime;
-    //alarm.play();
+  var now;
+  var interval = 0;
+  var clear = 0;
+  var minutes = 0; 
+  var seconds = 0;
+  var newTime;
+  var alarm = $('#alarm')[0];
+  var count1 = parseInt($('#count1').html());
+  var count1ToMinutes;
+  
+  var count2 = parseInt($('#count2').html());
+  var breakTime;
+  //alarm.play();
 
-    //amount of seconds total
-
-    function sessionTimer() {
-        if (count1 === 0) {
-            //alarm.play(); 
-            clear();
-            breakTime = setInterval(breakTimer, 1000);
-        } else {
-            count1--;
+  function breakTimer() {
+    //$('#count2').
+  }
+  
+  function sessionTimer() {
+    
+    if (count1ToMinutes === 0) {
+      alarm.play(); 
+      clear();
+      breakTime = setInterval(breakTimer, 1000);
+    } else {
+      count1ToMinutes--;
+      minutes = Math.floor(count1ToMinutes / 60);
+      console.log(minutes)
+      seconds = count1ToMinutes % 60;
+        if(seconds < 10) {
+        seconds = '0' + seconds;
         }
-
-        function breakTimer() {
-            //$('#count2').
-        }
-        /*
-    if(seconds < 10) {
-      seconds = '0' + seconds;
+      $('#countDown').html(minutes + ':' + seconds);
     }
-    */
+ 
+  };
+    
+//Buttons
+  
+ $('#start').click(function() {
+   interval = setInterval(sessionTimer, 1000);
+ });
 
-        // Display the result in the element with class=timer"
-        $('.timer').html(count1); 
-    };
+ $('#stop').click(function() {
+   clear = clearInterval(interval);
+ });
 
-
-    //Buttons
-
-    $('#start').click(function() {
-        interval = setInterval(sessionTimer, 1000);
-    });
-
-    $('#stop').click(function() {
-        clear = clearInterval(interval);
-    });
-    //$('#reset').click()
-
-    $('#minus1').click(function() {
-        if(count1 > 1) {
-            count1--;
-            $('#count1').html(count1);
-            $('#sessionTime').html(count1 + ':00');
-        }
-    });
-
-    $('#plus1').click(function() {
-        count1++;
-        $('#count1').html(count1); 
-        $('#sessionTime').html(count1 + ':00');
-    });
-
-    $('#minus2').click(function() {
-        if(count2 > 1) {
-            count2--;
-            $('#count2').html(count2);
-        }
-    });
-
-    $('#plus2').click(function() {
-        count2++;
-        $('#count2').html(count2); 
-    });
-
+$('#minus1').click(function() {
+  if(count1 > 1) {
+    count1--;
+    $('#count1').html(count1);
+    $('#countDown').html(count1 + ':00');
+    count1ToMinutes = count1 * 60;
+  }
 });
+
+$('#plus1').click(function() {
+  count1++;
+  $('#count1').html(count1); 
+  $('#countDown').html(count1 + ':00');
+   count1ToMinutes = count1 * 60;
+  console.log(count1ToMinutes)
+});
+
+$('#minus2').click(function() {
+  if(count2 > 1) {
+    count2--;
+    $('#count2').html(count2);
+  }
+});
+
+$('#plus2').click(function() {
+  count2++;
+  $('#count2').html(count2); 
+});
+  
+});
+
 /*
 $('#reset').click(function() {
     $('#count1').html()
 });
 */
-/*
 
-function Timer(callback, delay) {
-    var timerId, start, remaining = delay;
 
-    this.pause = function() {
-        window.clearTimeout(timerId);
-        remaining -= new Date() - start;
-    };
-
-    this.resume = function() {
-        start = new Date();
-        window.clearTimeout(timerId);
-        timerId = window.setTimeout(callback, remaining);
-    };
-
-    this.resume();
-}
-
-var timer = new Timer(function() {
-    alert("Done!");
-}, 1000);
-    */
