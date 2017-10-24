10b00f6878ce41c56ff9cb867fef366dd7aabed6
Mike Mullen <mmullen227@gmail.com>
2017-10-23 23:29:42 -0400

    break timer not functioning properly

--- advanced_front_end_projects/pomodoro_clock/script.js
+++ advanced_front_end_projects/pomodoro_clock/script.js
@@ -1,36 +1,63 @@
 $(document).ready(function() {
-  var now;
-  var interval = 0;
   var clear = 0;
+  var interval = 0;
   var minutes = 0; 
   var seconds = 0;
-  var newTime;
   var alarm = $('#alarm')[0];
-  var count1 = parseInt($('#count1').html());
-  var count1ToMinutes;
-  
+  var count1 = parseInt($('#count1').html());;
+  var count1ToMinutes= count1 * 60;
   var count2 = parseInt($('#count2').html());
+  var count2ToMinutes = count2 * 60;
   var breakTime;
+  var sessionTime;
+  var start = false;
+  var session;
+  var breaK;
+  var theCount;
   //alarm.play();
-
+  
+  function conversion() {
+    if(session) {
+      theCount = count1ToMinutes;
+    } else {
+      theCount = count2ToMinutes;
+    }
+      minutes = Math.floor(theCount / 60);
+      seconds = theCount % 60;
+       if(seconds < 10) {
+        seconds = '0' + seconds;
+        }
+  }
+  
   function breakTimer() {
     //$('#count2').
+    
+    if (count2ToMinutes === 0) {
+      alarm.play(); 
+      clear = clearInterval(interval);
+      $('#sessionBreak').html('Session');
+      sessionTime = setInterval(sessionTimer, 1000);
+    } else {
+      $('#countDown').html(count2 + ':00');
+      count2ToMinutes--;
+      session = false;
+      conversion();
+      $('#countDown').html(minutes + ':' + seconds);
+    }
   }
+  
   
   function sessionTimer() {
     
     if (count1ToMinutes === 0) {
       alarm.play(); 
-      clear();
+      clear = clearInterval(interval);
+      $('#sessionBreak').html('Break Time!');
       breakTime = setInterval(breakTimer, 1000);
     } else {
       count1ToMinutes--;
-      minutes = Math.floor(count1ToMinutes / 60);
-      console.log(minutes)
-      seconds = count1ToMinutes % 60;
-        if(seconds < 10) {
-        seconds = '0' + seconds;
-        }
+      session = true;
+      conversion();
       $('#countDown').html(minutes + ':' + seconds);
     }
  
@@ -39,11 +66,16 @@ $(document).ready(function() {
 //Buttons
   
  $('#start').click(function() {
-   interval = setInterval(sessionTimer, 1000);
+   if(!start) {
+    interval = setInterval(sessionTimer, 1000);
+    start = true;
+   }
+   
  });
 
  $('#stop').click(function() {
    clear = clearInterval(interval);
+   start = false;
  });
 
 $('#minus1').click(function() {
@@ -75,12 +107,15 @@ $('#plus2').click(function() {
   $('#count2').html(count2); 
 });
   
-});
 
-/*
 $('#reset').click(function() {
-    $('#count1').html()
+    $('#count1').html('25');
+    $('#count2').html('5');
+    $('#countDown').html('25:00');
+    clear = clearInterval(interval);
+    start = false;
+    //count1ToMinutes = count1 * 60;
 });
-*/
 
+});
 
