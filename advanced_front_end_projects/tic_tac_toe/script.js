7b8352917ead495a9df96ea2a11328ba1311d153
Mike Mullen <mmullen227@gmail.com>
2018-02-13 13:12:47 -0500

    modal working properly, assignPlayers function added, placeValuesOnBoard function added

--- advanced_front_end_projects/tic_tac_toe/script.js
+++ advanced_front_end_projects/tic_tac_toe/script.js
@@ -1,39 +1,47 @@
-$(document).ready(function() {
-  // variable where user selection is stored (X or O)
-  let $user
-  
-  // variable where computer selection is stored (X or O)
-  let $computer
-  
-  // Get the modal
-  const $modal = $('.modal');
-    
-  // Get the X or O <span> element 
-  const $selection = $('.selection');
-    
-  const $modalContent = $('.modal-content')
-  
-  // After the content in the window loads, run the modal
-  $modal.css('display', 'block');
-  
-  
-  // When the user clicks on <span> X or O, close the modal and store the value of whichever one was clicked
-    $($selection).click(function() {
-      $modal.css('display', 'none');
-      $user = $(this).val();
-    });
-    
-    $user == 'X' ? $computer = 'O' : $computer = 'X'
-    
-    console.log($computer)
+$(function () {
+    let user = '';
+    let computer = '';
+    const X = '<i class= "fas fa-times fa-7x"></i>';
+    const O = '<i class="far fa-circle fa-6x"></i>';
+    const wins = [ [0, 1, 2]];
+    let selectedTarget;
 
-    
-    $(document).click(function(event) {
-  //if you click on anything except the modal itself or the "open modal" link, close the modal
-  if (!$(event.target).closest($modalContent).length) {
-    $("body").find($modal).css('display', 'none');
-  }
-});
+    // Get the modal
+    const $modal = $('.modal');
 
-  
+    const modalContent = $('.modal-content')
+
+    // After the content in the window loads, run the modal
+    $modal.css('display', 'block');
+
+    // When the user clicks on button X or O, close the modal and store the value of whichever one was clicked. If something else is clicked, do nothing.
+    function assignPlayers(event) {
+        let $value = $(event.target).val();
+        if ($value == 'X') {
+            user = X;
+            computer = O
+            $modal.css('display', 'none');
+        } else if ($value == 'O') {
+            user = O;
+            computer = X
+            $modal.css('display', 'none');
+        }
+    }
+
+
+    function placeValuesOnBoard(event) {
+        let $target = $(event.target);
+        if ($target.has('i').length || $target.is('i')) {
+            console.log('test');
+            return;
+        }
+        $target.append(user);
+        console.log($target);
+    }
+
+    $('#myModal').click(assignPlayers);
+
+    $('table').click(placeValuesOnBoard);
+
+
 });
